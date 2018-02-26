5aed7441c003af9191a25b27d82659c0a471af95
Ahmadou Dicko <ahmadou.dicko@gmail.com>
2018-02-26 23:28:01 +0000

    add IBM Plex Sans

--- R/themes.R
+++ R/themes.R
@@ -45,63 +45,69 @@
 #'   theme_ocha(grid="Y") +
 #'   theme(axis.text.y=element_blank())
 #' }
-theme_ocha <- function(base_family = "Arial Narrow", base_size = 11.5,
-                      plot_title_family = base_family, plot_title_size = 18,
-                      plot_title_face = "bold", plot_title_margin = 10,
-                      subtitle_family = base_family, subtitle_size = 12,
-                      subtitle_face = "plain", subtitle_margin = 15,
-                      strip_text_family = base_family, strip_text_size = 12,
-                      strip_text_face = "plain",
-                      caption_family = base_family, caption_size = 9,
-                      caption_face = "italic", caption_margin = 10,
-                      axis_text_size = base_size,
-                      axis_title_family = subtitle_family, axis_title_size = 9,
-                      axis_title_face = "plain", axis_title_just = "rt",
-                      plot_margin = margin(30, 30, 30, 30),
-                      grid_col = "#cccccc", grid = TRUE,
-                      axis_col = "#cccccc", axis = FALSE, ticks = FALSE) {
-  
-  ret <- ggplot2::theme_minimal(base_family = base_family, base_size = base_size)
-  ret <- ret + theme(legend.background = element_blank())
-  ret <- ret + theme(legend.key = element_blank())
+theme_ocha <- function(
+  base_family="IBMPlexSans", base_size = 11.5,
+  plot_title_family="IBMPlexSans-Bold", plot_title_size = 18,
+  plot_title_face="plain", plot_title_margin = 10,
+  subtitle_family=if (.Platform$OS.type == "windows") "IBMPlexSans" else "IBMPlexSans-Light",
+  subtitle_size = 13,
+  subtitle_face = "plain", subtitle_margin = 15,
+  strip_text_family = "IBMPlexSans-Medium", strip_text_size = 12,
+  strip_text_face = "plain",
+  caption_family=if (.Platform$OS.type == "windows") "IBMPlexSans" else "IBMPlexSans-Thin",
+  caption_size = 9,
+  caption_face = "plain", caption_margin = 10,
+  axis_text_size = 9,
+  axis_title_family = base_family,
+  axis_title_size = 9,
+  axis_title_face = "plain",
+  axis_title_just = "rt",
+  plot_margin = margin(30, 30, 30, 30),
+  grid_col = "#cccccc", grid = TRUE,
+  axis_col = "#cccccc", axis = FALSE, ticks = FALSE) {
+
+  ret <- ggplot2::theme_minimal(base_family=base_family, base_size=base_size)
+
+  ret <- ret + theme(legend.background=element_blank())
+  ret <- ret + theme(legend.key=element_blank())
 
   if (inherits(grid, "character") | grid == TRUE) {
 
-    ret <- ret + theme(panel.grid = element_line(color = grid_col, size = 0.2))
-    ret <- ret + theme(panel.grid.major = element_line(color = grid_col, size = 0.2))
-    ret <- ret + theme(panel.grid.minor = element_line(color = grid_col, size = 0.15))
+    ret <- ret + theme(panel.grid=element_line(color=grid_col, size=0.2))
+    ret <- ret + theme(panel.grid.major=element_line(color=grid_col, size=0.2))
+    ret <- ret + theme(panel.grid.minor=element_line(color=grid_col, size=0.15))
 
     if (inherits(grid, "character")) {
-      if (regexpr("X", grid)[1] < 0) ret <- ret + theme(panel.grid.major.x = element_blank())
-      if (regexpr("Y", grid)[1] < 0) ret <- ret + theme(panel.grid.major.y = element_blank())
-      if (regexpr("x", grid)[1] < 0) ret <- ret + theme(panel.grid.minor.x = element_blank())
-      if (regexpr("y", grid)[1] < 0) ret <- ret + theme(panel.grid.minor.y = element_blank())
+      if (regexpr("X", grid)[1] < 0) ret <- ret + theme(panel.grid.major.x=element_blank())
+      if (regexpr("Y", grid)[1] < 0) ret <- ret + theme(panel.grid.major.y=element_blank())
+      if (regexpr("x", grid)[1] < 0) ret <- ret + theme(panel.grid.minor.x=element_blank())
+      if (regexpr("y", grid)[1] < 0) ret <- ret + theme(panel.grid.minor.y=element_blank())
     }
 
   } else {
-    ret <- ret + theme(panel.grid = element_blank())
+    ret <- ret + theme(panel.grid=element_blank())
   }
 
   if (inherits(axis, "character") | axis == TRUE) {
-    ret <- ret + theme(axis.line = element_line(color = "#2b2b2b", size = 0.15))
+    ret <- ret + theme(axis.line=element_line(color=axis_col, size=0.15))
     if (inherits(axis, "character")) {
       axis <- tolower(axis)
       if (regexpr("x", axis)[1] < 0) {
         ret <- ret + theme(axis.line.x=element_blank())
       } else {
-        ret <- ret + theme(axis.line.x=element_line(color = axis_col, size = 0.15))
+        ret <- ret + theme(axis.line.x=element_line(color=axis_col, size=0.15))
       }
       if (regexpr("y", axis)[1] < 0) {
-        ret <- ret + theme(axis.line.y = element_blank())
+        ret <- ret + theme(axis.line.y=element_blank())
       } else {
-        ret <- ret + theme(axis.line.y = element_line(color = axis_col, size = 0.15))
+        ret <- ret + theme(axis.line.y=element_line(color=axis_col, size=0.15))
       }
     } else {
-      ret <- ret + theme(axis.line.x = element_line(color = axis_col, size = 0.15))
-      ret <- ret + theme(axis.line.y = element_line(color = axis_col, size = 0.15))
+      ret <- ret + theme(axis.line.x=element_line(color=axis_col, size=0.15))
+      ret <- ret + theme(axis.line.y=element_line(color=axis_col, size=0.15))
     }
   } else {
-    ret <- ret + theme(axis.line = element_blank())
+    ret <- ret + theme(axis.line=element_blank())
   }
 
   if (!ticks) {
@@ -115,44 +121,77 @@ theme_ocha <- function(base_family = "Arial Narrow", base_size = 11.5,
     ret <- ret + theme(axis.ticks.length = grid::unit(5, "pt"))
   }
 
-  xj <- switch(tolower(substr(axis_title_just, 1, 1)), b = 0, l = 0, m = 0.5, c = 0.5, r = 1, t = 1)
-  yj <- switch(tolower(substr(axis_title_just, 2, 2)), b = 0, l = 0, m = 0.5, c = 0.5, r = 1, t = 1)
+  xj <- switch(tolower(substr(axis_title_just, 1, 1)), b=0, l=0, m=0.5, c=0.5, r=1, t=1)
+  yj <- switch(tolower(substr(axis_title_just, 2, 2)), b=0, l=0, m=0.5, c=0.5, r=1, t=1)
 
-  ret <- ret + theme(axis.text.x = element_text(size = axis_text_size, margin = margin(t = 0)))
-  ret <- ret + theme(axis.text.y = element_text(size = axis_text_size, margin = margin(r = 0)))
-  ret <- ret + theme(axis.title = element_text(size = axis_title_size, family = axis_title_family))
-  ret <- ret + theme(axis.title.x = element_text(hjust = xj, size = axis_title_size,
-                                               family = axis_title_family, face = axis_title_face))
-  ret <- ret + theme(axis.title.y = element_text(hjust = yj, size = axis_title_size,
-                                               family = axis_title_family, face = axis_title_face))
-  ret <- ret + theme(axis.title.y.right = element_text(hjust = yj, size = axis_title_size, angle = 90,
-                                                     family = axis_title_family, face = axis_title_face))
-  ret <- ret + theme(strip.text = element_text(hjust = 0, size = strip_text_size,
-                                             face = strip_text_face, family = strip_text_family))
-  ret <- ret + theme(panel.spacing = grid::unit(2, "lines"))
-  ret <- ret + theme(plot.title = element_text(hjust = 0, size = plot_title_size,
-                                             margin = margin(b = plot_title_margin),
-                                             family = plot_title_family, face = plot_title_face))
-  ret <- ret + theme(plot.subtitle = element_text(hjust = 0, size = subtitle_size,
-                                                margin = margin(b = subtitle_margin),
-                                                family = subtitle_family, face = subtitle_face))
-  ret <- ret + theme(plot.caption = element_text(hjust = 1, size = caption_size,
-                                               margin = margin(t = caption_margin),
-                                               family = caption_family, face = caption_face))
-  ret <- ret + theme(plot.margin = plot_margin)
+  ret <- ret + theme(axis.text.x=element_text(size=axis_text_size, margin=margin(t=0)))
+  ret <- ret + theme(axis.text.y=element_text(size=axis_text_size, margin=margin(r=0)))
+  ret <- ret + theme(axis.title=element_text(size=axis_title_size, family=axis_title_family))
+  ret <- ret + theme(axis.title.x=element_text(hjust=xj, size=axis_title_size,
+                                               family=axis_title_family, face=axis_title_face))
+  ret <- ret + theme(axis.title.y=element_text(hjust=yj, size=axis_title_size,
+                                               family=axis_title_family, face=axis_title_face))
+  ret <- ret + theme(axis.title.y.right=element_text(hjust=yj, size=axis_title_size, angle=90,
+                                                     family=axis_title_family, face=axis_title_face))
+  ret <- ret + theme(strip.text=element_text(hjust=0, size=strip_text_size,
+                                             face=strip_text_face, family=strip_text_family))
+  ret <- ret + theme(panel.spacing=grid::unit(2, "lines"))
+  ret <- ret + theme(plot.title=element_text(hjust=0, size=plot_title_size,
+                                             margin=margin(b=plot_title_margin),
+                                             family=plot_title_family, face=plot_title_face))
+  ret <- ret + theme(plot.subtitle=element_text(hjust=0, size=subtitle_size,
+                                                margin=margin(b=subtitle_margin),
+                                                family=subtitle_family, face=subtitle_face))
+  ret <- ret + theme(plot.caption=element_text(hjust=1, size=caption_size,
+                                               margin=margin(t=caption_margin),
+                                               family=caption_family, face=caption_face))
+  ret <- ret + theme(plot.margin=plot_margin)
 
   ret
 
 }
 
-#' Update matching font defaults for text geoms
+#' Import IBM Plex Sans font for use in charts
 #'
-#' Updates [ggplot2::geom_label] and [ggplot2::geom_text] font defaults
+#' IBM Plex Sans is a trademark of IBM and distributed under the SIL Open Font License, Version 1.1.
 #'
-#' @param family,face,size,color font family name, face, size and color
+#' There is an option `hrbrthemes.loadfonts` which -- if set to `TRUE` -- will
+#' call `extrafont::loadfonts()` to register non-core fonts with R PDF & PostScript
+#' devices. If you are running under Windows, the package calls the same function
+#' to register non-core fonts with the Windows graphics device.
+#'
+#' @md
+#' @note This will take care of ensuring PDF/PostScript usage. The location of the
+#'   font directory is displayed after the base import is complete. It is highly
+#'   recommended that you install them on your system the same way you would any
+#'   other font you wish to use in other programs.
 #' @export
-update_geom_font_defaults <- function(family = "Arial Narrow", face = "plain", size = 3.5,
-                                      color = "#2b2b2b") {
-  update_geom_defaults("text", list(family = family, face = face, size = size, color = color))
-  update_geom_defaults("label", list(family = family, face = face, size = size, color = color))
+import_plex_sans <- function() {
+
+  ps_font_dir <- system.file("fonts", "plex-sans", package = "ochathemes")
+
+  suppressWarnings(suppressMessages(extrafont::font_import(ps_font_dir, prompt=FALSE)))
+
+  message(
+    sprintf(
+      "You will likely need to install these fonts on your system as well.\n\nYou can find them in [%s]",
+      ps_font_dir)
+  )
+
 }
+
+#' @rdname PlexSans
+#' @md
+#' @title PlexSans font name R variable aliases
+#' @description `font_ps` == "`IBMPlexSans`"
+#' @format length 1 character vector
+#' @export
+font_ps <- "IBMPlexSans"
+
+#' @rdname PlexSans
+#' @md
+#' @note `font_ps_light` (a.k.a. "`IBMPlexSans-Light`") is not available on
+#'     Windows and will throw a warning if used in plots.
+#' @description `font_ps_light` == "`IBMPlexSans-Light`"
+#' @export
+font_ps_light <- "IBMPlexSans-Light"
